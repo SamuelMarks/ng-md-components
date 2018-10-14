5d039affa217d1c5a9897329d3b18642d28ddb4b
Samuel Marks <samuelmarks@gmail.com>
2018-10-15 00:41:26 +1100

    Parse out `templateUrl` in preparation for conversion

--- index.js
+++ index.js
@@ -1,42 +1,62 @@
 #!/usr/bin/env node
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var path = require("path");
-var walk = function (dir, done) {
-    var results = [];
-    fs.readdir(dir, function (err, files) {
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const walk = (dir, endswith, done) => {
+    let results = [];
+    fs.readdir(dir, (err, files) => {
         if (err)
             return done(err);
-        var pending = files.length;
+        let pending = files.length;
         if (!pending)
-            return done(null, results);
-        files.forEach(function (file) {
+            return done(void 0, results);
+        files.forEach((file) => {
             file = path.resolve(dir, file);
-            fs.stat(file, function (err, stats) {
+            fs.stat(file, (err, stats) => {
                 if (stats && stats.isDirectory()) {
-                    walk(file, function (err, res) {
-                        if (typeof res !== 'undefined')
-                            results = results.concat(res);
-                        if (!--pending)
-                            done(null, results);
-                    });
+                    if (['node_modules' /* TODO: parse .gitignore */].indexOf(path.basename(file)) > -1)
+                        !--pending && done(void 0, results);
+                    else
+                        walk(file, endswith, (err, res) => {
+                            if (typeof res !== 'undefined')
+                                results = results.concat(res);
+                            if (!--pending)
+                                done(void 0, results);
+                        });
                 }
                 else {
-                    results.push(file);
+                    if (file.length >= endswith.length && file.slice(file.length - endswith.length) === endswith)
+                        results.push(file);
                     if (!--pending)
-                        done(null, results);
+                        done(void 0, results);
                 }
             });
         });
     });
 };
+const neg1_to_max = (n) => n === -1 ? Number.MAX_SAFE_INTEGER : n;
+const parseTemplateUrl = (fname) => {
+    const data = fs.readFileSync(fname, 'utf8').replace(/\s/g, '');
+    const j = data.slice(data.indexOf('templateUrl') + 'templateUrl'.length + 4);
+    return j.slice(0, ((a, b) => a > b ? b : a)(neg1_to_max(j.indexOf('\'')), neg1_to_max(j.indexOf('"'))));
+};
 if (require.main === module) {
     if (process.argv.length < 3)
-        throw Error("Usage: " + process.argv + " PATH");
-    walk(process.argv[2], function (err, results) {
+        throw Error(`Usage: ${process.argv} PATH`);
+    walk(process.argv[2], '.component.ts', (err, fnames) => {
         if (err != null)
             throw err;
-        console.info(results);
+        else if (fnames == null || !fnames.length)
+            throw TypeError('fnames is empty');
+        const m = new Map(fnames.map(fname => [fname, parseTemplateUrl(fname)]));
+        console.info(m);
     });
 }
--- index.ts
+++ index.ts
@@ -3,36 +3,50 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const walk = (dir: string, done: (err: NodeJS.ErrnoException, results?: string[]) => void): void => {
+const walk = (dir: string, endswith: string, done: (err?: NodeJS.ErrnoException, results?: string[]) => void): void => {
   let results: string[] = [];
   fs.readdir(dir, (err: NodeJS.ErrnoException, files: string[]) => {
     if (err) return done(err);
     let pending = files.length;
-    if (!pending) return done(null, results);
+    if (!pending) return done(void 0, results);
     files.forEach((file: string) => {
       file = path.resolve(dir, file);
       fs.stat(file, (err: NodeJS.ErrnoException, stats: fs.Stats) => {
         if (stats && stats.isDirectory()) {
-          walk(file, (err: Error, res: string[]) => {
-            if (typeof res !== 'undefined')
-              results = results.concat(res);
-            if (!--pending) done(null, results);
-          });
+          if (['node_modules' /* TODO: parse .gitignore */].indexOf(path.basename(file)) > -1)
+            !--pending && done(void 0, results);
+          else
+            walk(file, endswith, (err?: NodeJS.ErrnoException, res?: string[]) => {
+              if (typeof res !== 'undefined')
+                results = results.concat(res);
+              if (!--pending) done(void 0, results);
+            });
         } else {
-          results.push(file);
-          if (!--pending) done(null, results);
+          if (file.length >= endswith.length && file.slice(file.length - endswith.length) === endswith)
+            results.push(file);
+          if (!--pending) done(void 0, results);
         }
       });
     });
   });
 };
 
+const neg1_to_max = (n: number): number => n === -1 ? Number.MAX_SAFE_INTEGER : n;
+
+const parseTemplateUrl = (fname: string): string => {
+  const data = fs.readFileSync(fname, 'utf8').replace(/\s/g, '');
+  const j = data.slice(data.indexOf('templateUrl') + 'templateUrl'.length + 4);
+  return j.slice(0, ((a, b) => a > b ? b : a)(neg1_to_max(j.indexOf('\'')), neg1_to_max(j.indexOf('"'))));
+};
+
 if (require.main === module) {
   if (process.argv.length < 3)
     throw Error(`Usage: ${process.argv} PATH`);
 
-  walk(process.argv[2], (err, results) => {
+  walk(process.argv[2], '.component.ts', (err, fnames) => {
     if (err != null) throw err;
-    console.info(results);
+    else if (fnames == null || !fnames.length) throw TypeError('fnames is empty');
+    const m = new Map<string, string>(fnames.map(fname => [fname, parseTemplateUrl(fname)]) as any);
+    console.info(m);
   });
 }
