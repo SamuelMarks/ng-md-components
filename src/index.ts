63468e4f4d15670fb3d83d5f27e656146184feab
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-10-15 17:27:08 +1100

    Regenerate scaffold

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,29 @@ import { Command, flags } from '@oclif/command';
 
 import { recursiveMd2Html } from './utils';
 
-
 class NgMdComponents extends Command {
-    static description = 'describe the command here';
+  static description = 'describe the command here';
 
-    static flags = {
-        // add --version flag to show CLI version
-        version: flags.version({ char: 'v' }),
-        help: flags.help({ char: 'h' }),
-        // flag with a value (-n, --name=VALUE)
-        directory: flags.string({ char: 'd', description: 'Directory to recurse through', required: true }),
-        ext: flags.string({ char: 'e', description: 'File extension to look for', default: '.md' }),
-    };
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+    directory: flags.string({ char: 'd', description: 'Directory to recurse through', required: true }),
+    ext: flags.string({ char: 'e', description: 'File extension to look for', default: '.md' }),
+  };
 
-    async run() {
-        /* tslint:disable:no-shadowed-variable */
-        const { args, flags } = this.parse(NgMdComponents);
+  static args = [{ name: 'file' }];
 
-        if (!(flags.ext as string).startsWith('.')) flags.ext = `.${flags.ext}`;
+  async run() {
+    /* tslint:disable:no-shadowed-variable */
+    const { args, flags } = this.parse(NgMdComponents);
 
-        recursiveMd2Html(flags.directory, flags.ext as string, err => {
-            if (err != null) throw err;
-        });
-    }
+    if (!(flags.ext as string).startsWith('.')) flags.ext = `.${flags.ext}`;
+
+    recursiveMd2Html(flags.directory, flags.ext as string, err => {
+      if (err != null) throw err;
+    });
+  }
 }
 
 export = NgMdComponents;
